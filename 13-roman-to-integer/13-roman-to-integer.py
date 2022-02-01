5785572c91dbbe0db369a4f04819e1e8fd88b0b5
Brian Sigilai <briansigilai@gmail.com>
2022-02-01 12:26:54 +0300

    Time: 36 ms (85.95%), Space: 13.5 MB (60.58%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,4 +1,4 @@
 class Solution(object):
     def romanToInt(self, s):
-        sum_arr = sum([int(i) for i in s.replace('CM','900 ').replace('M','1000 ').replace('CD','400 ').replace('D','500 ').replace('XC','90 ').replace('C','100 ').replace('XL','40 ').replace('L','50 ').replace('IX','9 ').replace('X','10 ').replace('IV','4 ').replace('V','5 ').replace('I','1 ').split()])
-        return sum_arr
+       
+        return sum([int(i) for i in s.replace('CM','900 ').replace('M','1000 ').replace('CD','400 ').replace('D','500 ').replace('XC','90 ').replace('C','100 ').replace('XL','40 ').replace('L','50 ').replace('IX','9 ').replace('X','10 ').replace('IV','4 ').replace('V','5 ').replace('I','1 ').split()])
