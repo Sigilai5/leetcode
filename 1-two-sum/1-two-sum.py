ceb92cd29c641140af0ebe27be972753a7072c36
Brian Sigilai <briansigilai@gmail.com>
2022-03-18 10:47:45 +0300

    Time: 3548 ms (28.16%), Space: 14.2 MB (69.95%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,9 @@
 class Solution(object):
     def twoSum(self, arr, target):
-        final = []
         for i in range(len(arr) - 1):
             for j in range(i+1,len(arr)):
                 if arr[i] + arr[j] == target:
-                    final.append(i)
-                    final.append(j)
-        return final
+                    return [i,j]
                 
                 
                 
