f656311d936b5c98f9c33762a877af35b61f2670
Brian Sigilai <briansigilai@gmail.com>
2022-03-18 10:38:56 +0300

    Time: 3948 ms (20.74%), Space: 14.3 MB (46.54%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,23 +1,18 @@
 class Solution(object):
-    def twoSum(self, nums, target):
-        index_arr = []
-        original_nums = [i for i in nums]
-        index_fin = []
-        for i in nums:
-            original_nums.remove(i)
-            for j in original_nums:
-                if i + j == target:
-                    index_arr.append(i)
-                    index_arr.append(j)
-        if index_arr[0] == index_arr[1]:
-            for n,i in enumerate(nums):
-                if i == index_arr[0]:
-                    index_fin.append(n)
-        else:
-            index_fin.append(nums.index(index_arr[0]))
-            index_fin.append(nums.index(index_arr[1]))
-
-        return index_fin
+    def twoSum(self, arr, target):
+        final = []
+        for i in range(len(arr) - 1):
+            for j in range(i+1,len(arr)):
+                if arr[i] + arr[j] == target:
+                    final.append(i)
+                    final.append(j)
+        return final
+                
+                
+                
+                    
+        
+        
                     
                     
             
