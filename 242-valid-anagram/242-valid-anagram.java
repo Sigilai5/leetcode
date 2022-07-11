a54230472e81f6b041d76669992b3133c52bdcb4
Brian Sigilai <briansigilai@gmail.com>
2022-07-11 16:20:20 +0300

    Time: 9 ms (42.71%), Space: 45.1 MB (33.94%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,13 +1,12 @@
-
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        
         char sArr[] = s.toCharArray();        
         char tArr[] = t.toCharArray();
         
         Arrays.sort(sArr);
         Arrays.sort(tArr);
-        
-        
         
         boolean compareArr = Arrays.equals(sArr,tArr);
         return compareArr;
