10d144023e411bf1756eb2218d57f4381dac7402
Brian Sigilai <briansigilai@gmail.com>
2022-07-12 07:44:32 +0300

    Time: 665 ms (30.82%), Space: 43.7 MB (57.95%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.replaceAll("[^a-zA-Z0-9]","");
+        s = s.toLowerCase();
+        s = s.replaceAll("\\s","");
+        char sArr[] = s.toCharArray();
+        char rArr[] = new char[sArr.length];
+        int pointer = 0;
+        for(int i = sArr.length - 1; i >= 0;i--){
+            rArr[pointer] = sArr[i];
+            pointer++;
+        }
+        
+        boolean isPalindrome = Arrays.equals(sArr,rArr);
+        return isPalindrome;
+        
+    }
+}
