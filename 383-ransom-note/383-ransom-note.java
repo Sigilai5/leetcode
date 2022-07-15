65bda2f4eb1cd4990c27e0c02f3ffd13f05f2535
Brian Sigilai <briansigilai@gmail.com>
2022-07-16 01:43:18 +0300

    Time: 3 ms (91.05%), Space: 47 MB (42.36%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,26 @@
+
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+       if(ransomNote == magazine) return true;
+        
+        int[] alphabetArr = new int[26]; //Memory location for all letters in the alphabet
+        
+        for(char c: magazine.toCharArray()){
+            alphabetArr[c - 'a']+=1;                   //By subtracting 'a' we are getting the index of character in the alphabet and increment
+        }
+        
+        for(char c: ransomNote.toCharArray()){
+            if(alphabetArr[c - 'a'] == 0){             //Check if character has value in the alphabetArr
+                return false;
+            }
+            
+            alphabetArr[c - 'a']-=1;
+        }
+        
+        
+        return true;
+        
+        
+    }
+}
