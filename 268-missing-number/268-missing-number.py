2f3ae40f97cd21679e188cc917b0f7e3f41678ce
Brian Sigilai <briansigilai@gmail.com>
2022-07-17 17:23:34 +0300

    Time: 3343 ms (5.04%), Space: 14.7 MB (42.31%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def missingNumber(self, nums):
-        num_range = range(0, len(nums)+1)
-        for i in num_range:
+        for i in range(0, len(nums)+1):
             if i not in nums:
                 return i
