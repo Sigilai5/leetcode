586a82a74520c84ef71d1c1930e8c4b15554cd3f
Brian Sigilai <briansigilai@gmail.com>
2022-07-25 08:54:32 +0300

    Time: 3 ms (14.29%), Space: 40.2 MB (91.20%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] s_arr = s.split("\\W+");
+        HashMap<Character,String> s_map = new HashMap();
+        boolean follow = true;
+        
+        if(s_arr.length != pattern.length()) return false;
+            
+        for(int i = 0; i < pattern.length(); i++){
+            
+            char current_char = pattern.charAt(i);
+            
+            if(s_map.containsKey(current_char)){
+                if(!s_map.get(current_char).equals(s_arr[i])){
+                    follow = false;
+                }
+                
+            } else {
+                if(s_map.containsValue(s_arr[i])){
+                    follow = false;
+                }
+            }
+            
+            
+            s_map.put(current_char,s_arr[i]);
+        }
+        
+    
+        
+        return follow;
+    }
+}
