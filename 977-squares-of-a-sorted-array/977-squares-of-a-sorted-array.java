80bd694c83d62727dbd292dee628e53d637b957f
Brian Sigilai <briansigilai@gmail.com>
2022-07-27 16:02:07 +0300

    Time: 558 ms (5.27%), Space: 45 MB (84.42%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -4,7 +4,15 @@ class Solution {
             nums[i] = nums[i] * nums[i];
         }
         
-        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+            if(nums[i] > nums[j]){
+                int hold = nums[i];
+                nums[i] = nums[j];
+                nums[j] = hold;    
+            }
+        }
+        }
         
         
         return nums;
