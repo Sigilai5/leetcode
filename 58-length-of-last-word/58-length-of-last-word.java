9422347d36532d5003095277ef75d268b2e8ec2d
Brian Sigilai <briansigilai@gmail.com>
2022-07-30 14:24:20 +0300

    Time: 2 ms (21.10%), Space: 42.4 MB (41.89%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] words = s.split(" ");
+        String last = words[words.length - 1];
+        
+        return last.length();
+    }
+}
