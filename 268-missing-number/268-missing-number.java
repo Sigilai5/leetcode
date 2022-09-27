5bd7eea24d0f51f53b922fe87ebf99a8ad544a05
Brian Sigilai <briansigilai@gmail.com>
2022-09-27 15:09:24 +0300

    Time: 25 ms (5.25%), Space: 53.6 MB (8.36%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,25 +1,50 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int[] arr_nums = new int[nums.length+1];
+          if(nums.length < 1) return -1;
+          if(nums.length == 1 && nums[0] == 0) return 1;  
+          if(nums.length == 1 && nums[0] == 1) return 0;  
+                 
+        
+          HashSet<Integer> missing = new HashSet();
+        
+          for(int i = 1; i <= nums.length; i++){   
+              missing.add(i);                         // [1,2,3]
+          }  
+        
+          for(int i: nums){                             //[3,0,1]
+              if(missing.contains(i)){
+                  missing.remove(i);                     //[2]
+              }
+          } 
         
         
-        for(int i = 0; i <= nums.length;i++){
-            arr_nums[i] = i;
-        }
+          if(missing.isEmpty()) return 0;  
+           
+          return missing.stream().findFirst().get();
         
-        HashMap<Integer,Integer> numsMap = new HashMap();
         
-        for(int num: nums){
-            numsMap.put(num,num);
-        }
         
-        for(int num: arr_nums){
-            if(!numsMap.containsKey(num)){
-                return num;
-            }
-        }
         
-        return 0;
+//         int[] arr_nums = new int[nums.length+1];
+        
+        
+//         for(int i = 0; i <= nums.length;i++){
+//             arr_nums[i] = i;
+//         }
+        
+//         HashMap<Integer,Integer> numsMap = new HashMap();
+        
+//         for(int num: nums){
+//             numsMap.put(num,num);
+//         }
+        
+//         for(int num: arr_nums){
+//             if(!numsMap.containsKey(num)){
+//                 return num;
+//             }
+//         }
+        
+//         return 0;
         
        
     }
