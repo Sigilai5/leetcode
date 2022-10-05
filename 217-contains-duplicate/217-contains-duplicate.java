b63550f6a263b7c37469c10e429793d9701b8ac7
Brian Sigilai <briansigilai@gmail.com>
2022-10-05 23:26:42 +0300

    Time: 27 ms (29.06%), Space: 51.4 MB (97.64%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,16 +1,38 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> uniques = new HashSet();
-        for(int num:nums){
-            if(uniques.contains(num)){
-                return true;
-            }
-            
-            uniques.add(num);
+        HashMap<Integer,Integer> duplicates = new HashMap();
+        for(int i: nums){
+            duplicates.put(i,duplicates.getOrDefault(i,0)+1);
         }
         
+        for(int i: duplicates.keySet()){
+            if(duplicates.get(i) > 1){
+                return true;
+            }
+        }
         
         return false;
-        
     }
 }
+
+
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashSet<Integer> uniques = new HashSet();
+//         for(int num:nums){
+//             if(uniques.contains(num)){
+//                 return true;
+//             }
+            
+//             uniques.add(num);
+//         }
+        
+        
+//         return false;
+        
+//     }
+// }
+
+//Space Complexity -> O(n)
+//Time Complexity -> O(n)
