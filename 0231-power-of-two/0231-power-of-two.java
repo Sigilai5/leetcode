8ed552b43d46efb4e5b78800fcb79801402eb1b4
Brian Sigilai <briansigilai@gmail.com>
2022-10-25 18:59:03 +0300

    Time: 2 ms (80.14%), Space: 41.7 MB (23.69%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n == 0) return false;
-        if(n == 1) return true;
+        if(n <= 0) return false;
+     
         
         while(n%2 == 0){
             n = n/2;
@@ -15,3 +15,8 @@ class Solution {
         
     }
 }
+
+
+
+//Space Complexity:O(logN)
+//Time COmplexity:O(1)
