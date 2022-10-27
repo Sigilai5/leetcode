aae8579aeb91505635e751f370abbfcca2e009a9
Brian Sigilai <briansigilai@gmail.com>
2022-10-27 23:31:48 +0300

    Time: 1 ms (86.10%), Space: 41.1 MB (52.86%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,21 +1,32 @@
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-     int count = 0;
-     int check = 1;
-     for(int i = 0; i < 32; i++){
-        if((n & check) != 0){
-            count++;
-        }
+//      int count = 0;
+//      int check = 1;
+//      for(int i = 0; i < 32; i++){
+//         if((n & check) != 0){
+//             count++;
+//         }
          
-        check = check << 1; 
-     } 
+//         check = check << 1; 
+//      } 
     
-     return count;
+//      return count;
+        
+    //Time Complexity: O(32) -> O(1)
+    //Space Complexity: O(1)
+        
+    /*Method 2->Brian Kernighan Algorithm,*/
+    int bitCount = 0;
+    while(n != 0){
+        n = n & (n-1); //The rightmost bit will be flipped each time until we end with a zero
+        bitCount+=1;        
+    }   
+        
+    return bitCount;
+   
        
     }
 }
 
 
-//Space Complexity: O(1)
-//Time Complexity: O(32) -> O(1)
