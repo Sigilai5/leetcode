3394b0672e2ad1b9176d5280ec0ad5297e7b62b3
Brian Sigilai <briansigilai@gmail.com>
2022-11-04 10:53:02 +0300

    Time: 2 ms (66.22%), Space: 42.1 MB (99.13%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -23,20 +23,51 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
+        /*BFS Iteratively*/
         if(root == null) return root;
+        Queue<Node> nodes = new LinkedList();
         
-        if(root.left != null){
-            root.left.next = root.right;
+        nodes.add(root);
+        
+        while(!nodes.isEmpty()){
+            Node head = nodes.poll();
+            
+            if(head.left != null){ 
+                head.left.next = head.right;
+            }
+            
+            if(head.right != null && head.next != null){
+                head.right.next = head.next.left;
+            }
+            
+            if(head.left != null && head.right != null){
+                 nodes.add(head.left);
+                nodes.add(head.right);
+            }
+           
+            
+            
         }
         
-        if(root.right != null && root.next != null){
-            root.right.next = root.next.left;
-        }
-        
-        connect(root.left);
-        connect(root.right);
         
         return root;
+        
+        
+        /*DFS Recursively*/
+//         if(root == null) return root;
+        
+//         if(root.left != null){
+//             root.left.next = root.right;
+//         }
+        
+//         if(root.right != null && root.next != null){
+//             root.right.next = root.next.left;
+//         }
+        
+//         connect(root.left);
+//         connect(root.right);
+        
+//         return root;
         
     }
     
