a1d2c2f6acb378a8232e41ed4b314c924048d139
Brian Sigilai <briansigilai@gmail.com>
2022-11-06 16:20:43 +0300

    Time: 4 ms (57.53%), Space: 75.9 MB (82.56%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,23 +2,29 @@ class Solution {
     public int maxProfit(int[] prices) {
     if(prices.length == 0) return 0;
     
-    int maxProfit = 0;
-    int left = 0;
-    int right = 1;
-    while(right < prices.length){
-        if(prices[right] > prices[left]){
-            int dif = prices[right] - prices[left];
-            maxProfit = Math.max(maxProfit,dif);
-        }else{
-            left = right;
-        }
+     int maxProfit = 0;
+     int buying = 0;
+     int selling = 1;
+        
+      while(selling < prices.length){
+          if(prices[selling] > prices[buying]){
+              maxProfit = Math.max(maxProfit,prices[selling] - prices[buying]);
+              
+          }else{
+              buying = selling;
+          }
           
-        right++;
+          selling+=1;
+          
+      }
         
         
-    }
+      return maxProfit;
         
-    return maxProfit;    
+        
+     //Space Complexity => O(1)
+     //Time Complexity => O(N)   
+        
   
   }
     
