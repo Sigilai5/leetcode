e83ce37da43f82de206bd4432dad5dd8765edee5
Brian Sigilai <briansigilai@gmail.com>
2022-11-07 18:05:12 +0300

    Time: 236 ms (39.43%), Space: 99.2 MB (76.85%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -69,11 +69,3 @@ class UndergroundSystem {
     
     
 }
-
-/**
- * Your UndergroundSystem object will be instantiated and called as such:
- * UndergroundSystem obj = new UndergroundSystem();
- * obj.checkIn(id,stationName,t);
- * obj.checkOut(id,stationName,t);
- * double param_3 = obj.getAverageTime(startStation,endStation);
- */
