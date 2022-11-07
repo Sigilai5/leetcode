aeb8b221f0f75a7c8beaad97dce86a5582d63104
Brian Sigilai <briansigilai@gmail.com>
2022-11-07 19:25:50 +0300

    Time: 233 ms (45.71%), Space: 104 MB (27.22%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -1,71 +1,92 @@
 class UndergroundSystem {
+    Map<Integer,Journey> journeys;   //Check in time
+    Map<String,Average> averages;           //Record total time and number of customers
     
-       private final String DELIMETER = ",";
-       
-       Map<Integer,Journey> journeys;
-       Map<String,Averages> averages;
-
+  
     
-    public UndergroundSystem() {
-       journeys = new HashMap<>();
-       averages = new HashMap<>();
-        
+    public UndergroundSystem() {   
+       journeys = new HashMap();
+       averages = new HashMap(); 
     }
     
     public void checkIn(int id, String stationName, int t) {
-      journeys.put(id,new Journey(id,stationName,t));
+        journeys.put(id,new Journey(id,stationName,t));
     }
     
     public void checkOut(int id, String stationName, int t) {
-      Journey getJourney = journeys.get(id);
-      journeys.remove(id);
-      
-      int timeDifference = t - getJourney.time;  
+    Journey journey = journeys.get(id);
+    journeys.remove(id);      //Journey is complete
         
-      String key = getJourney.stationName + DELIMETER + stationName;
+    int timeDifference = t - journey.t;
         
-      Averages average = averages.containsKey(key) ? averages.get(key) : new Averages();
+    String keyStation = journey.stationName + "," + stationName;    
         
-      average.updateAverage(timeDifference);
+    Average average = averages.containsKey(keyStation) ? averages.get(keyStation) : new Average();   
         
-      averages.put(key,average);  
+    average.updateAverage(timeDifference);  
+        
+    averages.put(keyStation,average);    
         
     }
     
     public double getAverageTime(String startStation, String endStation) {
-       String key = startStation + DELIMETER + endStation;
-       return averages.get(key).getAverages(); 
+      String keyStation = startStation + "," + endStation;  
+      return averages.get(keyStation).getAverage();  
+        
         
     }
     
-
-   class Journey {
-       private int id;
-       private String stationName; 
-       private int time;
-       
-       public Journey(int id, String stationName, int time){
-           this.id = id;
-           this.stationName = stationName;
-           this.time = time;
-       }
-   }
+    class Journey {
+        private int id;
+        private String stationName;
+        private int t;
+        
+        public Journey(int id, String stationName, int t){
+            this.id = id;
+            this.stationName = stationName;
+            this.t = t;
+        }
+        
+    }
     
     
-   class Averages {
-       private double totalTime = 0;
-       private int totalCustomers = 0;
-       
-       public void updateAverage(int timeDifference){
-           totalTime += timeDifference;
-           totalCustomers+=1;
-       }
-       
-       
-       public double getAverages(){
-           return totalTime / totalCustomers;
-       }
-   } 
+    class Average {
+        private double totalTime = 0;
+        private int customerCount = 0;
+        
+        public void updateAverage(int timeDifference){
+            totalTime += timeDifference;
+            customerCount++;
+            
+        }
+        
+        public double getAverage(){
+            return totalTime / customerCount;
+        }
+        
+    }
+ 
     
     
 }
+
+
+
+
+// Your UndergroundSystem object will be instantiated and called as such:
+// obj = UndergroundSystem()
+// obj.checkIn(id,stationName,t)
+// obj.checkOut(id,stationName,t)
+// param_3 = obj.getAverageTime(startStation,endStation)
+
+
+//A->B  3 
+//A->B  2
+//A->B  4
+
+//A->C
+
+//A->C
+
+
+//Average->(3+2+4)/3 -> 3
