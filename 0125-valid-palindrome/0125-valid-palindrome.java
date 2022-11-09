3709c24154f23713ec25c2aaddd17fec322f895d
Brian Sigilai <briansigilai@gmail.com>
2022-11-09 14:25:18 +0300

    Time: 1295 ms (14.85%), Space: 48 MB (19.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,41 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       if(s == "") return true;
+        s = s.replaceAll("[^a-zA-Z0-9]",""); //remove all non-alphanumeric characters.
+        s = s.toLowerCase();
+        // s = s.replaceAll("\\s","");  //Remove whitespaces
+        char sArr[] = s.toCharArray();
+        char rArr[] = new char[sArr.length];
+        int pointer = 0;
+        for(int i = sArr.length - 1; i >= 0;i--){
+            rArr[pointer] = sArr[i];
+            pointer++;
+        }
+        
+        boolean isPalindrome = Arrays.equals(sArr,rArr);
+        return isPalindrome;
+        
+        //Space Complexity -> O(m+n)
+        //Time Complexity -> O(n)
+        
+        /*Method 2*/
+//         if(s == "") return true;
        
-       String newString = s.toLowerCase();
-       String removedAlpha = newString.replaceAll("[^A-Za-z0-9]","");
+//        String newString = s.toLowerCase();
+//        String removedAlpha = newString.replaceAll("[^A-Za-z0-9]","");
        
-       char[] sArray = removedAlpha.toCharArray();
+//        char[] sArray = removedAlpha.toCharArray();
         
-       String finalString = ""; 
+//        String finalString = ""; 
         
-       for(int i = sArray.length-1; i>=0; i--){
-           finalString+= sArray[i];
-       } 
+//        for(int i = sArray.length-1; i>=0; i--){
+//            finalString+= sArray[i];
+//        } 
         
         
-      
+//         return finalString.equals(removedAlpha);
         
-        return finalString.equals(removedAlpha);
+        //Space Complexity -> O(n)
+        //Time Complexity -> O(n)
         
     }
 }
