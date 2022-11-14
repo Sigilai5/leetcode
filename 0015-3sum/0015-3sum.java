4ab45152ff3013ca5515919a79523c824dbeff6e
Brian Sigilai <briansigilai@gmail.com>
2022-11-14 12:57:12 +0300

    Time: 40 ms (51.92%), Space: 58.1 MB (79.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> triplets = new LinkedList<List<Integer>>();
-        Arrays.sort(nums);
+        ArrayList<List<Integer>> result = new ArrayList();
+        if(nums == null || nums.length < 3) return result;
+        
+        Arrays.sort(nums); //Sorting helps in shifting l and r pointers and avoiding duplicates
         
         for(int i = 0; i < nums.length;i++){
-            if(i > 0 && nums[i] == nums[i-1]){
-                continue;
+            if(i > 0 && nums[i] == nums[i-1]){  
+                continue;                      // skip duplicates e.g -3 and -3 in  [-3,-3,0,1,2,2,2]
             }
-             
-            int l = i+1;
-            int r = nums.length -1;
             
-            while(l < r){
-                int threeSum = nums[i] + nums[l] + nums[r];
-                if(threeSum > 0){
-                    r -= 1;
-                }else if(threeSum < 0){
-                    l += 1;
-                }else{
-                    triplets.add(Arrays.asList(nums[i],nums[l],nums[r]));
-                    l += 1;
-                    while(nums[l] == nums[l-1] && l < r){
-                        l += 1;
-                    }
-                }   
-            }
+            int first = nums[i];
+            int left = i+1;
+            int right = nums.length - 1;
+            
+            while(left < right){
+            int threeSum = first + nums[left] + nums[right];
+            if(threeSum > 0){
+                right--;
+            }else if(threeSum < 0){
+                left++;
+            }else{
+                result.add(Arrays.asList(first,nums[left],nums[right]));
+                left++;
+                while(nums[left] == nums[left-1] && left < right){
+                    left++;
+                }
+                }
+            } 
         }
         
-    
-        return triplets;
+        
+        return result;
+        
         
     }
 }
+
+// Time Complexity:O(n^2)
+// Space Complexity:O(n)
