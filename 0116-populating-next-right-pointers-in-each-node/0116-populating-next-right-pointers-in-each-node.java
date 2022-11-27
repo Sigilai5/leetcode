432b4a40159a41603ec7e68c3b666f57e5306c5f
Brian Sigilai <briansigilai@gmail.com>
2022-11-27 12:18:36 +0300

    Time: 0 ms (100.00%), Space: 46.8 MB (73.98%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,32 +1,54 @@
 class Solution {
     public Node connect(Node root) {
      if(root == null) return root;
+        //DFS RECURSIVELY
         
-     Queue<Node> level = new LinkedList(); //FIFO [1]   [2,3]
-     level.add(root); //[1]
+        if(root.left != null && root.right != null){ 
+            root.left.next = root.right;
+        }
         
-     while(!level.isEmpty()){
-         Node head = level.remove(); //[1]
+        if(root.right != null && root.next != null){
+            root.right.next = root.next.left;
+        }
+        
+        
+        connect(root.left);
+        connect(root.right);
+        
+        return root;
+        
+            //Time Complexity: O(h)
+            //Space Complexity: O(1) 
+        
+        
+        //BFS ITERATIVELY(BECAUSE OF QUE)
+//      Queue<Node> level = new LinkedList(); //FIFO [1]   [2,3]  [4,5,6,7]
+//      level.add(root); //[1]
+        
+//      while(!level.isEmpty()){
+//          Node head = level.remove(); //[1]
          
-         if(head.left != null && head.right != null){
-             head.left.next = head.right;   //[2->3]
-         }//done with left
+//          if(head.left != null && head.right != null){
+//              head.left.next = head.right;   //[2->3]
+//          }//done with left
          
-         if(head.right != null && head.next != null){
-             head.right.next = head.next.left;
-         }
+//          if(head.right != null && head.next != null){
+//              head.right.next = head.next.left;
+//          }
          
          
-         if(head.left != null && head.right != null){
-             level.add(head.left);
-             level.add(head.right);
-         }
+//          if(head.left != null && head.right != null){
+//              level.add(head.left);
+//              level.add(head.right);
+//          }
          
-     }
+//      }
         
      
-    return root;
+//     return root;
         
+//     //Time Complexity: O(h)
+//     //Space Complexity: O(n)    
         
    
         
