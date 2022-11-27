7e62c7ce7523b5119042fde16b653b69d9fbc08b
Brian Sigilai <briansigilai@gmail.com>
2022-11-27 12:12:00 +0300

    Time: 5 ms (41.54%), Space: 47.8 MB (23.34%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,80 +1,34 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
     public Node connect(Node root) {
-        /*BFS Iteratively*/
-        if(root == null) return root;
-        Queue<Node> nodes = new LinkedList();
+     if(root == null) return root;
         
-        nodes.add(root);
+     Queue<Node> level = new LinkedList(); //FIFO [1]   [2,3]
+     level.add(root); //[1]
         
-        while(!nodes.isEmpty()){
-            Node head = nodes.poll();
-            
-            if(head.left != null){ 
-                head.left.next = head.right;
-            }
-            
-            if(head.right != null && head.next != null){
-                head.right.next = head.next.left;
-            }
-            
-            if(head.left != null && head.right != null){
-                 nodes.add(head.left);
-                nodes.add(head.right);
-            }
-           
-            
-            
-        }
+     while(!level.isEmpty()){
+         Node head = level.remove(); //[1]
+         
+         if(head.left != null && head.right != null){
+             head.left.next = head.right;   //[2->3]
+         }//done with left
+         
+         if(head.right != null && head.next != null){
+             head.right.next = head.next.left;
+         }
+         
+         
+         if(head.left != null && head.right != null){
+             level.add(head.left);
+             level.add(head.right);
+         }
+         
+     }
         
-        
-        return root;
-        
-        //Space Complexity -> O(n);
-        //Time Complexity -> O(h)
+     
+    return root;
         
         
-        /*DFS Recursively*/
-//         if(root == null) return root;
-        
-//         if(root.left != null){
-//             root.left.next = root.right;
-//         }
-        
-//         if(root.right != null && root.next != null){
-//             root.right.next = root.next.left;
-//         }
-        
-//         connect(root.left);
-//         connect(root.right);
-        
-//         return root;
-         //Space Complexity -> O(1)
-    //Time Complexity -> O(h)
-        
-    }
-    
    
+        
+    }       
 }
