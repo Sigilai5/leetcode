f416181491d2980d745661b2388a661bd574d3a3
Brian Sigilai <briansigilai@gmail.com>
2022-11-29 10:47:24 +0300

    Time: 18 ms (85.27%), Space: 43.2 MB (88.56%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,32 +1,34 @@
 class Solution {
     public String frequencySort(String s) {
-        if(s.length() == 1) return s;
+        if(s.length() <= 1) return s;
         
         HashMap<Character,Integer> map = new HashMap();
         
         for(char c: s.toCharArray()){
             map.put(c,map.getOrDefault(c,0)+1);
-        }
+        } //{t=1,r=1,e=2}
+        
         
         
         PriorityQueue<HashMap.Entry<Character,Integer>> pq = new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
         
         for(HashMap.Entry entry: map.entrySet()){
-            pq.add(entry);     //{e=2,r=1,t=1}       
+            pq.add(entry); //[e=2,t=1,r=1]
         }
         
         
-        String output = ""; 
-        while(!pq.isEmpty()){ 
-            HashMap.Entry c = pq.poll();
-            for(int i = 0; i < map.get(c.getKey()); i++){ 
-                output+= c.getKey();
+        StringBuilder output = new StringBuilder();
+        
+        while(!pq.isEmpty()){
+            HashMap.Entry priority = pq.poll();
+            for(int j = 0; j < map.get(priority.getKey()); j++){
+                output.append(priority.getKey());
             } 
-            
         }
-         
         
-        return output;
+        
+        return output.toString();
+        
         
     }
 }
