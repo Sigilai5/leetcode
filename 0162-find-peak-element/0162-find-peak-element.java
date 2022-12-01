a60e7d3e1a7e26d14cff8f296c7e1fed315e1877
Brian Sigilai <briansigilai@gmail.com>
2022-12-01 13:21:35 +0300

    Time: 7 ms (6.08%), Space: 43.4 MB (12.94%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
+        
+        for(int num:nums){
+            pq.add(num);
+        }
+        
+        
+        int max = pq.poll();
+        
+        
+        int pointer = 0;
+        
+        while(pointer < nums.length){
+            if(nums[pointer] == max){
+                return pointer;
+            }
+            
+            pointer++;
+        }
+        
+        
+        return -1;
+        
+    }
+}
