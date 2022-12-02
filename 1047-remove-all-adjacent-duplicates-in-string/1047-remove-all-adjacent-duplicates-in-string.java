351176122b96c6f5c65632b1cff38c6201854a26
Brian Sigilai <briansigilai@gmail.com>
2022-12-02 18:03:48 +0300

    Time: 692 ms (10.46%), Space: 318.9 MB (16.53%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,26 +1,33 @@
 class Solution {
     public String removeDuplicates(String s) {
-       if(s.isEmpty()) return s;
-       Stack<Character> stack = new Stack();  
+        if(s.length() == 0) return s;
         
-       for(char c: s.toCharArray()){
-           if(!stack.isEmpty() && stack.peek() == c){
-               stack.pop();
-           }else{
-               stack.push(c);
-           }
-       } 
+        Stack<Character> stack = new Stack();
         
-       String output = new String();
+        for(char c: s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek() == c){
+                stack.pop();
+            }else{
+                stack.push(c);
+            }
+            
+        }
+        
+        String output = "";
         
         for(char c: stack){
             output+=c;
         }
         
+        
         return output;
         
     }
 }
+ 
+
+
+
 
 
 //Space Complexity -> O(n) since we are using a stack.
