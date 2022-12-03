1eb7b0946ebc735ff19cb4c3bb35e1299dcd38b0
Brian Sigilai <briansigilai@gmail.com>
2022-12-03 21:38:08 +0300

    Time: 2 ms (98.97%), Space: 42.6 MB (88.40%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {      
+        //*Option 2*
+        
+        int[] sArray = new int[26];
+        int[] tArray = new int[26];
+        
+        for(char c : s.toCharArray()){
+            sArray[c - 'a']++; 
+        }
+        
+        
+         for(char c : t.toCharArray()){
+            tArray[c - 'a']++;
+        }
+        
+        if(Arrays.equals(sArray,tArray)){
+            return true;
+        }
+        
+        
+        return false;
+        
+        
+        
+        //*Option 1*
+//         HashMap<Character,Integer> sCount = new HashMap();
+//         HashMap<Character,Integer> tCount = new HashMap();
+        
+//         for(char c: s.toCharArray()){
+//             sCount.put(c,sCount.getOrDefault(c,0)+1);
+//         }
+        
+        
+//         for(char c: t.toCharArray()){
+//             tCount.put(c,tCount.getOrDefault(c,0)+1);
+//         }
+        
+        
+//         return sCount.equals(tCount);
+        
+        
+        //Space Complexity->O(n+m)
+        //Time Complexity->O(n+m)
+
+    }
+}
+
+
