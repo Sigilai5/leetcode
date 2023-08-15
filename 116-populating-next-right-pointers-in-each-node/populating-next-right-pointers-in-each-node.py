e87bc96e18c962ec78790a29f8e6579f315fc1fc
Brian Sigilai <briansigilai@gmail.com>
2023-08-15 22:56:12 +0300

    Time: 70 ms (64.59%) | Memory: 18 MB (66.77%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return root
+        
+
+        if root.left:
+            root.left.next = root.right
+        
+        if root.right and root.next:
+            root.right.next = root.next.left
+        
+        self.connect(root.left)
+        self.connect(root.right)
+
+
+        return root
+        
+        # SC -> O(1), However if we include the recursion stack frames it would be O(N)
+        # TC -> O(N)
