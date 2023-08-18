8092ea0f3dbbc9060752f857cbbfde6208b14443
Brian Sigilai <briansigilai@gmail.com>
2023-08-18 10:33:57 +0300

    Time: 187 ms (93.68%) | Memory: 23.5 MB (85.03%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,24 +1,38 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        product_left = [0 for _ in range(len(nums))]
-        product_right = [0 for _ in range(len(nums))]
+        prefix_product = [0 for i in range(len(nums))]
 
-        product_left[0] = 1
+        prefix = 1
+        for i in range(len(nums)):
+            prefix_product[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums) - 1, -1,-1):
+            prefix_product[i] *= postfix
+            postfix *= nums[i]
+        
+        return prefix_product
 
-        for i in range(1,len(nums)):
-            product_left[i] = nums[i-1] * product_left[i-1]
+        # SC -> O(1)
+        # TC -> O(N)
         
 
-        product_right[len(nums) - 1] = 1
-
-        for j in range(len(nums) - 2, -1,-1):
-            product_right[j] = nums[j+1] * product_right[j+1]
-
-        for i in range(len(nums)):
-            nums[i] = product_right[i] * product_left[i]
 
 
-        return nums        
 
-        # SC -> O(N)
-        # TC -> O(N)
+
+
+
+    
+
+
+
+
+
+
+
+    # nums = [1,2,3,4]
+    # prefix_product = [1,1,2,6]
+    
+    # output = [24,12,8,6]
