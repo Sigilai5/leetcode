252ceb48be193aa4234bed910fc49246194dbf9d
Brian Sigilai <briansigilai@gmail.com>
2023-08-30 08:07:34 +0300

    Time: 298 ms (12.56%) | Memory: 19.3 MB (8.25%) - LeetSync

--- 1582-design-browser-history/design-browser-history.py
+++ 1582-design-browser-history/design-browser-history.py
@@ -1,30 +1,71 @@
-class BrowserHistory:
 
-    def __init__(self, homepage: str):
-        self.pages = []
-        self.pages.append(homepage)
-        self.current = 0
+# USING A LIST AND A POINTER
+# class BrowserHistory:
+
+#     def __init__(self, homepage: str):
+#         self.pages = []
+#         self.pages.append(homepage)
+#         self.current = 0
         
 
-    def visit(self, url: str) -> None:
-        while(len(self.pages) -1 != self.current):
-            self.pages.pop(-1)
-        self.pages.append(url)
-        self.current+=1
+#     def visit(self, url: str) -> None:
+#         while(len(self.pages) -1 != self.current):
+#             self.pages.pop(-1)
+#         self.pages.append(url)
+#         self.current+=1
         
 
-    def back(self, steps: int) -> str:
-        self.current = max(0,self.current - steps)
-        return self.pages[self.current]
+#     def back(self, steps: int) -> str:
+#         self.current = max(0,self.current - steps)
+#         return self.pages[self.current]
         
 
-    def forward(self, steps: int) -> str:
-        self.current = min(self.current + steps, len(self.pages) - 1)
-        return self.pages[self.current]
+#     def forward(self, steps: int) -> str:
+#         self.current = min(self.current + steps, len(self.pages) - 1)
+#         return self.pages[self.current]
         
 
     # SC -> O(N)
     # TC -> O(1)
+
+# USING A LINKED LIST
+
+class ListNode:
+    def __init__(self,val):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.dummy = ListNode(homepage)
+        self.curr = self.dummy
+        
+    def visit(self, url: str) -> None:
+        node = ListNode(url)
+        self.curr.next = node
+        node.prev = self.curr
+        self.curr = node
+
+    def back(self, steps: int) -> str:
+        for _ in range(steps):
+            if self.curr.prev != None:
+                self.curr = self.curr.prev
+        
+        return self.curr.val
+        
+
+    def forward(self, steps: int) -> str:
+        for _ in range(steps):
+            if self.curr.next != None:
+                self.curr = self.curr.next
+        
+        return self.curr.val
+
+# TC -> O(N)
+# SC -> O(N)
+
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
 # obj.visit(url)
