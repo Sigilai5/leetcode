78c1ac6841684c3525cb0e72d8ce20e41367afeb
Brian Sigilai <briansigilai@gmail.com>
2023-08-31 21:38:39 +0300

    Time: 176 ms (74.17%) | Memory: 16.6 MB (82.78%) - LeetSync

---
+++ 676-implement-magic-dictionary/implement-magic-dictionary.py
@@ -0,0 +1,26 @@
+class MagicDictionary:
+
+    def __init__(self):
+        self.words = []        
+
+    def buildDict(self, dictionary: List[str]) -> None:
+        self.words = dictionary        
+
+    def search(self, searchWord: str) -> bool:
+        for word in self.words:
+            if len(word) == len(searchWord):
+                count = 0
+                for i in range(len(word)):
+                    if word[i] != searchWord[i]:
+                        count+=1
+                
+                if count == 1: return True
+        
+        return False
+        
+
+
+# Your MagicDictionary object will be instantiated and called as such:
+# obj = MagicDictionary()
+# obj.buildDict(dictionary)
+# param_2 = obj.search(searchWord)
