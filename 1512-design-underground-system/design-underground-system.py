e748a39e673851ec888e78e8fffe8ee6ea06811f
Brian Sigilai <briansigilai@gmail.com>
2023-09-04 22:01:49 +0300

    Time: 237 ms (51.12%) | Memory: 28.6 MB (5.65%) - LeetSync

--- 1512-design-underground-system/design-underground-system.py
+++ 1512-design-underground-system/design-underground-system.py
@@ -1,47 +1,49 @@
 class UndergroundSystem:
 
     def __init__(self):
-        self.journeys = {}  # For each traveler
-        self.averages = {}  # For each station
+        self.checkins = {}
+        self.averages = {}
         
-
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.journeys[id] = self.Journey(id,stationName,t)
+        self.checkins[id] = self.Checkins(id,stationName,t)
         
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        timeDif = t - self.journeys[id].startTime
-        stations = self.journeys[id].startStation + '-' + stationName
+        timeDif = t - self.checkins[id].t
+        stations = self.checkins[id].stationName + '->' + stationName
         average = self.averages.get(stations,self.Average())
         average.updateAverage(timeDif)
-
         self.averages[stations] = average
         
-    def getAverageTime(self, startStation: str, endStation: str) -> float:
-        stations = startStation + '-' + endStation
-        return self.averages[stations].getAverage() 
 
-    class Journey:
-        def __init__(self, id,stationName,t):
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        stations = startStation + '->' + endStation
+        return self.averages[stations].getAverage()
+
+
+    class Checkins:
+
+        def __init__(self,id: int, stationName: str, t:int):
             self.id = id
-            self.startStation = stationName
-            self.startTime = t
-    
+            self.stationName = stationName
+            self.t = t
+
     
     class Average:
+
         def __init__(self):
             self.totalTime = 0
             self.customers = 0
         
         def updateAverage(self,timeDif):
-            self.totalTime+=timeDif
-            self.customers+= 1
+            self.totalTime += timeDif
+            self.customers += 1
         
         def getAverage(self):
-            return self.totalTime / self.customers
-    
-    # SC -> O(J + A)/ O(N + M)
-    # TC -> O(1)
+            return self.totalTime/self.customers
 
+    # SC -> O(N + M)
+    # TC -> O(1)
+        
 
 
 # Your UndergroundSystem object will be instantiated and called as such:
