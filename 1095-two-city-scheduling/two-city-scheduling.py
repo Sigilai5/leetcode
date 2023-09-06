981c95f2951242f330e7fc2965d27314681914a3
Brian Sigilai <briansigilai@gmail.com>
2023-09-06 22:56:46 +0300

    Time: 53 ms (42.34%) | Memory: 16.3 MB (58.08%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,29 +1,17 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        costs.sort(key = lambda x:x[0] - x[1])
-        
+        costs.sort(key= lambda x: x[0] - x[1])
+
+        total = 0
         mid_point = len(costs) // 2
-        initial_sum = 0
 
-        for i in range(0,len(costs) // 2):
-            initial_sum += costs[i][0]
-        
+        for i in range(mid_point):
+            total += costs[i][0]
+            total += costs[i+mid_point][1]
 
-        for i in range(len(costs) // 2, len(costs)):
-            initial_sum += costs[i][1]
-        
+        return total
 
-        return initial_sum
+        # SC -> O(1)
+        # TC -> O(N Log N)
 
-
-        # Space Complexity -> O(1)
-        # Time Complexity -> O(N Log N)
-
-        # sort by start value
-        # get the mid point
-        # initialize a sum of zero
-        # loop through array upto len(costs) // 2
-        # find sum of the first indices
-        # loop through array from len(costs)//2 to the last index
-        # find sum of the second indices
         
