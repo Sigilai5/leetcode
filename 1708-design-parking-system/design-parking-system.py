ab19fda189c052a0c0a8843e126b79e008a2a0fc
Brian Sigilai <briansigilai@gmail.com>
2023-09-11 23:26:55 +0300

    Time: 109 ms (95.64%) | Memory: 16.9 MB (46.08%) - LeetSync

--- 1708-design-parking-system/design-parking-system.py
+++ 1708-design-parking-system/design-parking-system.py
@@ -1,10 +1,7 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.parking = {}
-        self.parking[1] = big
-        self.parking[2] = medium
-        self.parking[3] = small
+        self.parking = {1:big,2:medium,3:small}
          
     def addCar(self, carType: int) -> bool:
         if self.parking[carType] > 0:
