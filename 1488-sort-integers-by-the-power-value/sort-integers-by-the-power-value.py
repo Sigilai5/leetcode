34fb5c4cc56b2379c2c804854a5fe1d4b41303ac
Brian Sigilai <briansigilai@gmail.com>
2023-09-18 22:22:06 +0300

    Time: 627 ms (22.45%) | Memory: 16.4 MB (95.14%) - LeetSync

--- 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
@@ -3,8 +3,8 @@ class Solution:
         num_list = []
         powers = {}
 
-        for i in range(lo,hi+1):
-            power = i
+        for x in range(lo,hi+1):
+            power = x
             steps = 0
 
             while power != 1:
@@ -15,57 +15,10 @@ class Solution:
                     power = 3 * power + 1
                     steps+=1
             
-            num_list.append(i)
-            powers[i] = steps
+            num_list.append(x)
+            powers[x] = steps
         
 
-        heap = []
+        num_list.sort(key = lambda x:powers[x])
 
-        for num, power in powers.items():
-            heapq.heappush(heap,(power,num))
-
-        for i in range(k-1):
-            heapq.heappop(heap)
-        
-        return heapq.heappop(heap)[1]
-
-
-
-    # SC -> O(N)
-    # TC -> O(NLogK)
-
-
-
-# Solutio with TC -> O(N Log N)
-
-# class Solution:
-#     def getKth(self, lo: int, hi: int, k: int) -> int:
-#         num_list = []
-#         powers = {}
-
-#         for i in range(lo,hi+1):
-#             power = i
-#             steps = 0
-
-#             while power != 1:
-#                 if power % 2 == 0:
-#                     power = power / 2
-#                     steps+=1
-#                 else:
-#                     power = 3 * power + 1
-#                     steps+=1
-            
-#             num_list.append(i)
-#             powers[i] = steps
-        
-
-#         num_list.sort(key = lambda x:powers[x])
-
-#         return num_list[k - 1]
-
-    # SC -> O(N)
-    # TC -> O(NLogN)
-
-        
-
-        
+        return num_list[k - 1]
