bab4030cecca9c4440f31155c745ac26464448c2
Brian Sigilai <briansigilai@gmail.com>
2023-10-30 15:09:40 +0300

    Time: 35 ms (89.11%) | Memory: 16.4 MB (81.82%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -4,8 +4,10 @@ class Solution:
 
         for num in nums:
             subset = [lst + [num] for lst in output]
-            print(subset)
             output+= subset
             
         return output
+
+        # SC -> O(2^N)
+        # TC -> O(N*(2^N))
         
