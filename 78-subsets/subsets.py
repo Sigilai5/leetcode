b0364010f23ef9aa4acbc8dfac3599560a71f9b4
Brian Sigilai <briansigilai@gmail.com>
2023-10-30 15:00:23 +0300

    Time: 36 ms (82.43%) | Memory: 16.6 MB (19.98%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,25 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(result,nums,subset,start):
-            result.append(subset[:])
+        output = [[]]
 
-            for i in range(start,len(nums)):
-                subset.append(nums[i])
-
-                backtrack(result,nums,subset,i+1)
-
-                subset.pop(-1)
-
+        for num in nums:
+            subset = [lst + [num] for lst in output]
+            print(subset)
+            output+= subset
+            
+        return output
         
-
-        result = []
-        subset = []
-
-        nums.sort()
-
-        backtrack(result,nums,subset,0)
-
-        return result
-        
-        # TC -> O(2^N)
-        # SC - O(N)
