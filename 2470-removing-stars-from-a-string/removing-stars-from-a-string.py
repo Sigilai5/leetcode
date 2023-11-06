6a46c59264cc7dad8d07ed59de821d4894080967
Brian Sigilai <briansigilai@gmail.com>
2023-11-06 23:24:09 +0300

    Time: 174 ms (86.78%) | Memory: 17.7 MB (73.66%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -6,28 +6,13 @@ class Solution:
             if char != "*":
                 stack.append(char)
             else:
-                stack.append(char)
-                for i in range(2):
-                    stack.pop()
-            
-
-        output = ""
-
-        while stack:
-            output+=stack.pop()
-
+                stack.pop()
         
-        return output[::-1]
+        return "".join(stack)
 
         # TC -> O(N)
         # SC -> O(N)
+        
 
-
-        # initialize a stack
-        # loop through the string
-        # if char is not * append to the stack
-        # otherwise append char and pop twice
-        # create an outupt string
-        # use a while loop to pop stack until its empty
-        # return output
+       
         
