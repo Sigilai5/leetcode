6515bd0cf4176f65eaf8ad121e564f5c3a564085
Brian Sigilai <briansigilai@gmail.com>
2023-11-17 23:28:19 +0300

    Time: 50 ms (58.63%) | Memory: 17.8 MB (20.68%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s_output =[char for char in s.lower().replace(" ","") if char.isalnum()]
+
+        return s_output == s_output[::-1]
+        
