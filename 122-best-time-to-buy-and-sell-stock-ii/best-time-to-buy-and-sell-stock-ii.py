6264a72dc507bd5013913edf1a508381aebf3963
Brian Sigilai <briansigilai@gmail.com>
2023-12-04 23:17:13 +0300

    Time: 58 ms (88.44%) | Memory: 17.8 MB (38.32%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -4,15 +4,16 @@ class Solution:
         max_profit = 0
 
         while sell < len(prices):
-            if prices[buy] >= prices[sell]:
-                sell+=1
-                buy+=1
-            else:
+            if prices[buy] < prices[sell]:
                 profit = prices[sell] - prices[buy]
-                max_profit += profit
-                buy+=1
-                sell+=1
+                max_profit += profit             
+            buy+=1
+            sell+=1
 
         return max_profit
+
+
+        # SC -> O(1)
+        # TC -> O(N)
 
         
