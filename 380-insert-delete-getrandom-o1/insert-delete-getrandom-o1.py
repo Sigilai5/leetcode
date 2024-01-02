d0eb9e674c5f1cddf3ff6745e940ec58cf12c0d2
Brian Sigilai <briansigilai@gmail.com>
2024-01-02 15:41:38 +0300

    Time: 810 ms (5.04%) | Memory: 64.5 MB (12.67%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,42 +1,35 @@
+import random
+
 class RandomizedSet:
 
     def __init__(self):
-        self.val_index = {}
-        self.vals = []
+        self.items = set()
         
     def insert(self, val: int) -> bool:
-        if val in self.val_index:
+        if val in self.items:
             return False
-        else:
-            self.val_index[val] = len(self.vals)
-            self.vals.append(val)
-            return True
+        
+        self.items.add(val)
+        return True
         
 
     def remove(self, val: int) -> bool:
-        if val not in self.val_index:
+        if val not in self.items:
             return False
-        else:
-            last_item = self.vals[-1]
-            idx = self.val_index.get(val)
-
-            # delete in dictionary
-            self.val_index[last_item] = idx
-            self.val_index.pop(val)
-
-            # delete from list
-            self.vals[idx] = last_item
-            self.vals.pop(-1)
-
-            return True
         
-
+        self.items.remove(val)
+        return True
+        
     def getRandom(self) -> int:
-        rand_idx = random.randint(0,len(self.vals)  -1)
+        items_list = list(self.items)
+        random_index = random.randint(0,len(items_list) - 1)
 
-        return self.vals[rand_idx]
+        return items_list[random_index]
         
 
 
- # SC -> O(N)
- # TC -> O(1)
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
