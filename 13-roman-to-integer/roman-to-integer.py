f9fc5b16448b149cb0e69c31870b0a4c0b760c91
Brian Sigilai <briansigilai@gmail.com>
2024-01-15 20:34:31 +0300

    Time: 48 ms (69.69%) | Memory: 17.3 MB (21.31%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -2,36 +2,30 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         roman_num = {
             "M":1000,
-            "CM":900,
             "D":500,
-            "CD":400,
             "C":100,
-            "XC":90,
             "L":50,
-            "XL":40,
             "X":10,
-            "IX":9,
             "V":5,
-            "IV":4,
             "I":1}
-
-        output = 0
 
         if len(s) == 1: return roman_num.get(s)
 
+        output = 0
+
         for i in range(1,len(s)):
-            if roman_num.get(s[i-1]) < roman_num.get(s[i]):
-                output-=roman_num.get(s[i-1])
-            else:
+            if roman_num.get(s[i-1]) >= roman_num.get(s[i]):
                 output+=roman_num.get(s[i-1])
+            else:
+                output-=roman_num.get(s[i-1])
+        
 
-
-        return output + roman_num.get(s[i])
-
+        return output + roman_num.get(s[-1])    
 
         # SC -> O(1)
-
         # TC -> O(N)
+
+        
 
             
         
