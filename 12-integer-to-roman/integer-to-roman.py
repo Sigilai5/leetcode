01b2bfca5e6013eebd4b2984b15c3e0431a4bb87
Brian Sigilai <briansigilai@gmail.com>
2024-01-15 20:47:19 +0300

    Time: 48 ms (71.96%) | Memory: 17.3 MB (35.87%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,56 +1,21 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        # num = 1994 -> MXC
-        # 994
-        # 94
-        # 4
-        output = ""
+        value_symbol = {1000:"M",900:"CM",500:"D",400:"CD",100:"C",90:"XC",50:"L",
+                     40:"XL",10:"X",9:"IX",5:"V",4:"IV",1:"I"}
 
-        while num != 0:
-            if num >= 1000:
-                output+="M"
-                num = num - 1000
-            elif num >= 900:
-                output+="CM"
-                num = num - 900
-            elif num >= 500:
-                output+="D"
-                num = num - 500
-            elif num >= 400:
-                output+="CD"
-                num = num - 400
-            elif num >= 100:
-                output+="C"
-                num = num - 100
-            elif num >= 90:
-                output+="XC"
-                num = num - 90
-            elif num >= 50:
-                output+="L"
-                num = num - 50
-            elif num >= 40:
-                output+="XL"
-                num = num - 40
-            elif num >= 10:
-                output+="X"
-                num = num - 10
-            elif num >= 9:
-                output+="IX"
-                num = num - 9
-            elif num >= 5:
-                output+="V"
-                num = num - 5
-            elif num >= 4:
-                output+="IV"
-                num = num - 4
-            elif num >= 1:
-                output+="I"
-                num = num - 1
-            
+        output = ""
+        
+        for value, symbol in value_symbol.items():
+            while num >= value:
+                output+=symbol
+                num = num - value
+        
 
         return output
 
 
 
-        
+        # SC -> O(1)
+        # TC -> O(N)
+
         
