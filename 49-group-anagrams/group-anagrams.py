77e04e29072f4c2652df4652850dd27aabbdaafb
Brian Sigilai <briansigilai@gmail.com>
2024-02-07 23:58:56 +0300

    Time: 86 ms (76.65%) | Memory: 20.1 MB (69.82%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if len(strs) <= 1: return [strs]
-
-        group = defaultdict(list)
-
+        if len(strs) <= 1:
+            return [strs]
+        
+        result = defaultdict(list)
 
         for st in strs:
             sorted_st = "".join(list(sorted(st)))
 
-            group[sorted_st].append(st)
+            result[sorted_st].append(st)
         
 
-        return group.values()
+        return result.values()
 
         # SC -> O(N)
         # TC -> O(N Log N)
