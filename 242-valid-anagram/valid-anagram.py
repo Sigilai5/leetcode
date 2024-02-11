c500967833634e907020ffa722652f8644798625
Brian Sigilai <briansigilai@gmail.com>
2024-02-11 19:58:28 +0300

    Time: 57 ms (31.63%) | Memory: 17.7 MB (26.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,9 +2,6 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return list(sorted(s)) == list(sorted(t)) 
 
-        # TC -> O(NLog(N))
         # SC -> O(N)
-
-
-
+        # TC -> O(N Log N)
         
