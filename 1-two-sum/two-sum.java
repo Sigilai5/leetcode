6b0ac98f052afd37841220a8bab2c05634f2e337
Brian Sigilai <briansigilai@gmail.com>
2024-03-19 23:13:22 +0300

    Time: 97 ms (5.04%) | Memory: 44.7 MB (74.06%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -4,8 +4,8 @@ class Solution {
         int[] result = new int[2]; 
 
         for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
+            for(int j = 0; j < nums.length; j++){
+                if(i!=j && nums[i] + nums[j] == target){
                     result[0] = i;
                     result[1] = j;
                  
@@ -17,4 +17,7 @@ class Solution {
         return result;
         
     }
+
+    // SC -> O(1)
+    // TC -> O(N*N)
 }
