61cfdb64992a113d4df7d721c3048bcdf22f8437
Brian Sigilai <briansigilai@gmail.com>
2024-03-22 08:38:13 +0300

    Time: 1 ms (99.46%) | Memory: 44.8 MB (74.30%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,28 +1,24 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        // Intialize and empty array
-        // loop through the range(n+1)
-        // check if num/15,if so append FizzBuzz,do the same for 5 and 3
-        // else append string num
-        // return the array
-
-        ArrayList<String> result = new ArrayList();
+        ArrayList<String> answer = new ArrayList();
 
         for(int i = 1; i <= n;i++){
-            if((i%15) == 0){
-                result.add("FizzBuzz");
-            }else if((i%5) == 0){ 
-                result.add("Buzz");
-            }else if((i%3) == 0){
-                result.add("Fizz");
+            if(i%3 == 0 && i%5 == 0){
+                answer.add("FizzBuzz");
+            }else if(i%3 == 0){
+                answer.add("Fizz");
+            }else if(i%5 == 0){
+                answer.add("Buzz");
             }else{
-                result.add(Integer.toString(i));
+                answer.add(Integer.toString(i));
             }
         }
 
-        return result;
-        
+
+        return answer;
+
     }
+
 }
 
 // SC -> O(N)
