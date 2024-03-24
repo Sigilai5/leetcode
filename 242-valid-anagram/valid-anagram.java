5461d710f97531af3fcb897f1905e3c19296b268
Brian Sigilai <briansigilai@gmail.com>
2024-03-24 22:15:03 +0300

    Time: 5 ms (53.09%) | Memory: 42.2 MB (99.78%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
+        if(s.length() != t.length()) return false;
+           
 
         int[] store = new int[26];
 
