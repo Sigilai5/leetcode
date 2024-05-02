d8c587b331357e445bc19d62aa14b2fece257e3c
Brian Sigilai <briansigilai@gmail.com>
2024-05-03 00:40:39 +0300

    Time: 1 ms (98.67%) | Memory: 41.4 MB (43.75%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()%2 != 0) return false;
-
         Stack<Character> stack = new Stack();
 
         for(char c: s.toCharArray()){
-            if(c == '(' || c == '{' || c == '[' ){
+            if(c == '(' || c == '[' || c == '{'){
                 stack.push(c);
-            } else if (!stack.isEmpty() && stack.peek() == '(' && c == ')'){
+            } else if(!stack.isEmpty() &&  c == ')' &&  stack.peek() == '('){
                 stack.pop();
-            } else if (!stack.isEmpty() && stack.peek() == '{' && c == '}'){
+            } else if(!stack.isEmpty() &&  c == ']' && stack.peek() == '['){
                 stack.pop();
-            }else if (!stack.isEmpty() && stack.peek() == '[' && c == ']'){
+            } else if(!stack.isEmpty() &&  c == '}' && stack.peek() == '{'){
                 stack.pop();
-            } else{
-                return false;
+            } else {
+                return false; 
             }
         }
 
