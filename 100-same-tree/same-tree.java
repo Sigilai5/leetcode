026509b14d9688a704c8dc71007b9cc865766ade
Brian Sigilai <briansigilai@gmail.com>
2024-05-15 18:57:22 +0300

    Time: 0 ms (100.00%) | Memory: 40.9 MB (47.56%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,18 +15,10 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null){
-            return true;
-        } else if(p == null || q == null || p.val != q.val){
-            return false;
-        } else {
-            return isSameTree(p.left, q.left) && isSameTree(p.right,q.right);
-        }
+        if(p == null && q == null) return true;
+        else if(p != null && q != null && p.val == q.val) return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
 
+        return false;
         
-
     }
 }
-
-// SC -> O(1), while ignoring recursive stack freames
-// TC -> O(N)
