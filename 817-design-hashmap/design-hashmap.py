10fcdf9e44517563ff77cbdcf97f85b77a9e1858
Brian Sigilai <briansigilai@gmail.com>
2024-07-19 23:06:45 +0300

    Time: 1989 ms (5.02%) | Memory: 19.7 MB (85.97%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -7,15 +7,18 @@ class MyHashMap:
     def put(self, key: int, value: int) -> None:
         for i in range(len(self.key_val)):
             if key == self.key_val[i][0]:
-                self.key_val[i][1] = value  # Update value if key exists
+                self.key_val[i][1] = value
                 return
-        self.key_val.append([key, value])  # Add new key-value pair if key does not exist
+        
+        new = [key,value]
+        self.key_val.append(new)
             
 
     def get(self, key: int) -> int:
         for i in range(len(self.key_val)):
             if key == self.key_val[i][0]:
                 return self.key_val[i][1]
+        
         return -1
         
 
@@ -23,13 +26,17 @@ class MyHashMap:
         for i in range(len(self.key_val)):
             if key == self.key_val[i][0]:
                 del self.key_val[i]
-                return
+                return 
+                
+        
     
     # SC -> O(N)
     # TC -> O(N)
+        
+
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
-# obj.put(key, value)
+# obj.put(key,value)
 # param_2 = obj.get(key)
 # obj.remove(key)
