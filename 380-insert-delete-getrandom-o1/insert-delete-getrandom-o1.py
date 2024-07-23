1d2b9ce141703adcc2103c871dd603226246f3a1
Brian Sigilai <briansigilai@gmail.com>
2024-07-23 22:07:52 +0300

    Time: 257 ms (69.57%) | Memory: 63.9 MB (30.40%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,27 +1,38 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.unique = set()
-        
+        self.key_val = {}
+        self.vals = []        
+
     def insert(self, val: int) -> bool:
-        if val in self.unique: return False
+        if val in self.key_val: return False
 
-        self.unique.add(val)
-        return True        
-
-    def remove(self, val: int) -> bool:
-        if val not in self.unique: return False
-
-        self.unique.remove(val)
+        self.key_val[val] = len(self.vals)
+        self.vals.append(val)
         return True
         
 
-    def getRandom(self) -> int:
-        result = list(self.unique)
-        return random.choice(result)
+    def remove(self, val: int) -> bool:
+        if val not in self.key_val: return False
+
+        # get last item and index to be deleted
+        last_item = self.vals[-1]
+        idx = self.key_val.get(val)
+
+        self.key_val[last_item] = idx
+        del self.key_val[val]
         
-    # SC -> O(N)
-    # TC -> O(N)
+        self.vals[idx] = last_item
+        self.vals.pop(-1)
+        
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.vals)
+        
+
+# SC -> O(N)
+# TC -> O(1)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
