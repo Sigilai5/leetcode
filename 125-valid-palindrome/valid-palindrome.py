771e6b5656bb5b2dfa09236c5e4510df4c70ecc8
Brian Sigilai <briansigilai@gmail.com>
2024-08-01 23:47:42 +0300

    Time: 47 ms (45.66%) | Memory: 17.3 MB (40.95%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -8,5 +8,18 @@ class Solution:
             if char.isalnum():
                 s_arr.append(char)
 
-        return s_arr == s_arr[::-1]
+        left = 0
+        right = len(s_arr) - 1
+
+        while left < right:
+            if s_arr[left] != s_arr[right]:
+                return False
+            
+            left+=1
+            right-=1
+
+        return True
+
+        # SC -> O(N)
+        # TC -> O(N)
         
