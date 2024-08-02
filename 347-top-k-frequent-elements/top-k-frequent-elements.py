97a1762371a28f987f175dac445f53b3bb3a7632
Brian Sigilai <briansigilai@gmail.com>
2024-08-02 23:11:35 +0300

    Time: 91 ms (46.30%) | Memory: 21.3 MB (41.57%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,29 +1,28 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_count  = {}
-
-        # count occurence
+        # get count for individual num
+        num_count = {}
         for num in nums:
             num_count[num] = num_count.get(num,0)+1
-
         
-        elements = [(-val,key) for key, val in num_count.items()]
+        heap = []
 
-        heapq.heapify(elements)
+        # use a 2D heap to store max values
+        for key,val in num_count.items():
+            heapq.heappush(heap,(-val,key))
+        
+        # create an empty array
+        result = []
 
-        top = []
-
+        # loop k  times
         for _ in range(k):
-            top.append(heapq.heappop(elements)[1])
+            if heap:
+                result.append(heapq.heappop(heap)[1])
         
-        return top
+
+        return result
 
         # SC -> O(N)
         # TC -> O(N Log K)
-
-
-        
-
-
 
         
