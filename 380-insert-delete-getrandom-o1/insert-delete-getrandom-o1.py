c7ced13a09a642a25826478b097c02ad372e2701
Brian Sigilai <briansigilai@gmail.com>
2024-08-07 23:53:37 +0300

    Time: 472 ms (15.13%) | Memory: 63.8 MB (33.86%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,35 +1,25 @@
 class RandomizedSet:
-
+    
     def __init__(self):
-        self.key_val = {}
-        self.vals = []        
+        self.unique = set()
 
     def insert(self, val: int) -> bool:
-        if val in self.key_val: return False
+        if val in self.unique: return False
 
-        self.key_val[val] = len(self.vals)
-        self.vals.append(val)
+        self.unique.add(val)
         return True
         
 
     def remove(self, val: int) -> bool:
-        if val not in self.key_val: return False
+        if val not in self.unique: return False
 
-        # get last item and index to be deleted
-        last_item = self.vals[-1]
-        idx = self.key_val.get(val)
-
-        self.key_val[last_item] = idx
-        del self.key_val[val]
-        
-        self.vals[idx] = last_item
-        self.vals.pop(-1)
-        
-        return True
+        self.unique.remove(val)
+        return True      
 
     def getRandom(self) -> int:
-        return random.choice(self.vals)
-        
+        result = list(self.unique)
+        randIdx = random.randint(0,len(result) - 1)
+        return result[randIdx]
 
 # SC -> O(N)
 # TC -> O(1)
