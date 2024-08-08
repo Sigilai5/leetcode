06613274a92af9cf3f98680a493beb8fad86de94
Brian Sigilai <briansigilai@gmail.com>
2024-08-08 22:48:34 +0300

    Time: 511 ms (97.76%) | Memory: 77.4 MB (88.94%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,26 +1,33 @@
+from collections import OrderedDict
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = OrderedDict()
         self.capacity = capacity
+        self.ord_dict = OrderedDict()
         
-
     def get(self, key: int) -> int:
-        if key in self.cache:
-            val = self.cache.get(key)
-            self.cache.pop(key)
-            self.cache[key] = val
-            return val
+        if key not in self.ord_dict: return -1
+
+        item = self.ord_dict.get(key)
+
+        self.ord_dict.pop(key)
+
+        self.ord_dict[key] = item
+
+        return item
         
-        return -1       
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.pop(key)
-        if len(self.cache) >= self.capacity:
-            self.cache.popitem(last=False)
+        if key in self.ord_dict:
+            self.ord_dict.pop(key)
+        if len(self.ord_dict) >= self.capacity:
+            self.ord_dict.popitem(last=False)
 
-        self.cache[key] = value
+        self.ord_dict[key] = value
+
+    # SC -> O(N)
+    # TC -> O(1)
         
 
 
