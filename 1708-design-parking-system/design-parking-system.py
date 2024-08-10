baa1ee4757595be2b397030764f3a64d590c1975
Brian Sigilai <briansigilai@gmail.com>
2024-08-10 21:09:28 +0300

    Time: 105 ms (46.98%) | Memory: 17 MB (74.14%) - LeetSync

--- 1708-design-parking-system/design-parking-system.py
+++ 1708-design-parking-system/design-parking-system.py
@@ -1,14 +1,30 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.parking = {1:big,2:medium,3:small}
-         
+        self.cars = {}
+        self.cars[1] = big
+        self.cars[2] = medium
+        self.cars[3] = small        
+
     def addCar(self, carType: int) -> bool:
-        if self.parking[carType] > 0:
-            self.parking[carType]  = self.parking.get(carType) - 1
-            return True
-        else:
-            return False
+        if carType in self.cars:
+            if carType == 1 and self.cars.get(carType) > 0:
+                self.cars[carType] = self.cars.get(carType) - 1
+                return True
+            elif carType == 2 and self.cars.get(carType) > 0:
+                self.cars[carType] = self.cars.get(carType) - 1
+                return True
+            elif carType == 3 and self.cars.get(carType) > 0:
+                self.cars[carType] = self.cars.get(carType) - 1
+                return True
+            else:
+                return False
+    
+    # SC -> O(1)
+    # TC -> O(1)
+
+
+
         
 
 
