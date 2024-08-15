0aaf03308ef49bda299424462856a0b228100f1d
Brian Sigilai <briansigilai@gmail.com>
2024-08-15 23:32:26 +0300

    Time: 251 ms (49.63%) | Memory: 18.9 MB (77.66%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,28 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-
         count = 0
 
-        for i in range(rows):
-            for j in range(cols):
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == "1":
-                    count +=  self.sink(grid,i,j)
-
-
+                    count += self.sink(grid,i,j)
+        
         return count
-
-    def sink(self,grid: List[List[str]],i: int,j: int):
-        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
+    
+    def sink(self,grid,i,j):
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]) or grid[i][j] != "1":
             return 0
-                
+        
         grid[i][j] = "0"
         self.sink(grid,i+1,j)
         self.sink(grid,i-1,j)
         self.sink(grid,i,j+1)
         self.sink(grid,i,j-1)
         return 1
-
+    
     # SC -> O(1)
-    # TC -> O(M * N)
+    # TC -> O(N*M)
+        
