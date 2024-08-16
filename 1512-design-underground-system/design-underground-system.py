8f2dceebb5c3e211176c1459f506e0c8c82d11f3
Brian Sigilai <briansigilai@gmail.com>
2024-08-16 23:43:25 +0300

    Time: 211 ms (31.76%) | Memory: 26 MB (97.84%) - LeetSync

--- 1512-design-underground-system/design-underground-system.py
+++ 1512-design-underground-system/design-underground-system.py
@@ -1,53 +1,54 @@
 class UndergroundSystem:
 
     def __init__(self):
-        self.journeys = {}
+        self.checkins = {}
         self.averages = {}
         
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.journeys[id] = self.Journey(id,stationName,t)        
-
+        self.checkins[id] = self.Checkin(id,stationName,t)
+        
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        timeDif = t - self.journeys[id].t
-        stations = self.journeys[id].stationName + "," + stationName
+        timeDif = t - self.checkins[id].t
+        stations = self.checkins[id].stationName + "," + stationName
 
         average = self.averages.get(stations,self.Average())
 
-        average.updateAverage(timeDif)    
+        average.updateAverage(timeDif)
 
         self.averages[stations] = average
-
-        self.journeys.pop(id)    
+        
+        self.checkins.pop(id)
+        
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
         stations = startStation + "," + endStation
         return self.averages[stations].getAverage()
 
     
-    class Journey:
-        def __init__(self,id: int,stationName: str,t: int):
+
+    class Checkin:
+
+        def __init__(self, id: int, stationName: str, t: int):
             self.id = id
             self.stationName = stationName
             self.t = t
-    
+
     class Average:
         def __init__(self):
             self.totalTime = 0
             self.customers = 0
 
         def updateAverage(self,timeDif):
-            self.totalTime += timeDif
-            self.customers += 1
-
+            self.totalTime+=timeDif
+            self.customers+=1
+        
         def getAverage(self):
-            return self.totalTime / self.customers
+            return self.totalTime/self.customers
+        
 
 # SC -> O(N+M)
 # TC -> O(1)
-
-        
-
 
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
