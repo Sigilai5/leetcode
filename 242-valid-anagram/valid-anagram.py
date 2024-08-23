6ea20ed6e0bc309ae5f45cdaafc2c138870d6afa
Brian Sigilai <briansigilai@gmail.com>
2024-08-23 23:49:03 +0300

    Time: 49 ms (56.93%) | Memory: 16.9 MB (88.63%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -7,17 +7,10 @@ class Solution:
 
         for i in range(len(s)):
             s_arr[ord(s[i]) - ord('a')]+=1
-
-        for i in range(len(t)):
             t_arr[ord(t[i]) - ord('a')]+=1
-
         
         return s_arr == t_arr
             
-        
-    
-
-    
 
 # SC -> O(1)
 # TC -> O(N)
