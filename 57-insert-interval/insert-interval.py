7e3041e95a3692fb9ac64984505a3c241eee3660
Brian Sigilai <briansigilai@gmail.com>
2024-09-02 23:34:50 +0300

    Time: 89 ms (5.52%) | Memory: 19.9 MB (16.33%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,16 +1,15 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # [[1,3],[2,5],[6,9]] -> [[1,5],[6,9]]
         intervals.append(newInterval)
 
-        intervals.sort(key = lambda x: x[0])
+        intervals.sort(key=lambda x:x[0])
 
-        i, j = 0, 1
-        
+        i = 0
+        j = 1
+
         while j < len(intervals):
             if intervals[j][0] <= intervals[i][1]:
-                max_val = max(intervals[i][1],intervals[j][1])
-                intervals[i][1] = max_val
+                intervals[i][1] = max(intervals[i][1],intervals[j][1])
                 intervals.pop(j)
             else:
                 i+=1
@@ -18,9 +17,7 @@ class Solution:
         
 
         return intervals
-
-        # TC -> O(N Log N)
-        # SC -> O(1)
-
-
         
+
+        # SC -> O(1)
+        # TC -> O(N Log N)
