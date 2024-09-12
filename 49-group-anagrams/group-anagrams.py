3b95e144d52ab9c04ad0826d9e82403d59786406
Brian Sigilai <briansigilai@gmail.com>
2024-09-12 22:58:01 +0300

    Time: 102 ms (16.61%) | Memory: 22.1 MB (23.82%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,14 +1,17 @@
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = defaultdict(list)
-
-        for word in strs:
-            let_group = [0] * 26
-
-            for i in range(len(word)):
-                let_group[ord(word[i]) - ord('a')]+=1
-            
-            groups[tuple(let_group)].append(word)
+    def groupAnagrams(self, anagrams: List[str]) -> List[List[str]]:
+        anagramsGroup = defaultdict(list)
         
-        return groups.values()
+        for word in anagrams:
+            alphabet_positions = [0] * 26
+            for i in range(len(word)):
+                alphabet_positions[ord('a') - ord(word[i])]+=1 # i = b 
+                
+            anagramsGroup[tuple(alphabet_positions)].append(word)
+        
+        return anagramsGroup.values()
+
+
+        # SC -> O(N)
+        # TC -> O(N * M)
         
