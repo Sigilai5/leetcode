69a82cf0c8ae73921a5762a8de40099a7d9eea16
Brian Sigilai <briansigilai@gmail.com>
2024-09-30 22:55:19 +0300

    Time: 53 ms (34.84%) | Memory: 16.9 MB (92.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,17 +2,16 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t): return False
 
-        sGrams = [0] * 26
-        tGrams = [0] * 26
-
+        sGram = [0] * 26
+        tGram = [0] * 26
 
         for i in range(len(s)):
-            sGrams[ord('a') - ord(s[i])]+=1
-            tGrams[ord('a') - ord(t[i])]+=1
+            sGram[ord('a') - ord(s[i])]+=1
+            tGram[ord('a') - ord(t[i])]+=1
         
+        return sGram == tGram
 
-        return sGrams == tGrams
 
-    # SC -> O(26)
-    # TC -> O(N)
+        # SC -> O(26) or O(1)
+        # TC -> O(N)
         
