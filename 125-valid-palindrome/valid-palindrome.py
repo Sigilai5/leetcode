187c850ef640e3e9bf0246b506385979fde14868
Brian Sigilai <briansigilai@gmail.com>
2024-10-09 22:43:14 +0300

    Time: 35 ms (95.16%) | Memory: 17.9 MB (26.94%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,16 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.replace(" ","").lower()
-
+        s = s.replace(" ","")
+        s = s.lower()
         result = []
 
         for ch in s:
             if ch.isalnum():
                 result.append(ch)
         
+
         return result == result[::-1]
-
-    # TC -> O(N)
-    # SC -> O(N)
-
         
