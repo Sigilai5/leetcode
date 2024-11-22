b29821d293d9c384d61796ed7a974668c8b60fca
Brian Sigilai <briansigilai@gmail.com>
2024-11-22 17:46:51 +0300

    Time: 2 ms (30.40%) | Memory: 17.3 MB (57.80%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -3,17 +3,16 @@ class Solution:
         output = []
 
         for i in range(1,n+1):
-            if i % 15 == 0:
+            if i%15 == 0:
                 output.append("FizzBuzz")
-            elif i % 5 == 0:
+            elif i%5 == 0:
                 output.append("Buzz")
-            elif i % 3 == 0:
+            elif i%3 ==0:
                 output.append("Fizz")
             else:
                 output.append(str(i))
         
         return output
-
 
         # SC -> O(N)
         # TC -> O(N)
