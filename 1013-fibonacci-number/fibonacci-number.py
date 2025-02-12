d232d16503b259dad281664491f06e98ef80c96b
Brian Sigilai <briansigilai@gmail.com>
2025-02-12 22:49:31 +0300

    Time: 371 ms (12.87%) | Memory: 17.9 MB (23.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,14 +1,10 @@
 class Solution:
     def fib(self, n: int) -> int:
-        # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233
+        # 0,1,1,2,3,5,8,13
+        if n == 0: return 0
+        elif n == 1: return 1
+        else: return self.fib(n-1) + self.fib(n-2)
 
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            return self.fib(n-1) + self.fib(n-2)
-
-        # SC -> O(1), if ignoring recursion stack frames
-        # TC -> O(2^N),exponential
+        # SC -> O(N), recursive stack frames
+        # TC -> O(N)
         
