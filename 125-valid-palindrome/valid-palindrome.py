0c58ac729f2edb86f4cd49e5d93b27534ff5d1de
Brian Sigilai <briansigilai@gmail.com>
2025-04-25 20:31:31 +0300

    Time: 5 ms (87.29%) | Memory: 18.9 MB (31.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,5 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        if len(s) == 0: return True
+
         s = s.replace(" ","")
         s = s.lower()
 
@@ -11,7 +13,16 @@ class Solution:
 
         
 
-        return output == output[::-1]
+        left,right = 0, len(output) - 1
+
+        while left < right:
+            if output[left] != output[right]:
+                return False
+            left+=1
+            right-=1
+
+        
+        return True
 
         # SC -> O(N)
         # TC -> O(N*N)        
