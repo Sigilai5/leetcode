4e1384eaf3a83794ed12bf18783c740190d954c4
Brian Sigilai <briansigilai@gmail.com>
2025-05-03 00:37:33 +0300

    Time: 27 ms (64.33%) | Memory: 29.2 MB (90.64%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -2,7 +2,7 @@ class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         slow,fast = nums[0],nums[0]
 
-        # Check if there is cycle
+        # Check if there is a cycle by slow x1 and fast x2
         while True:
             slow = nums[slow] # [4]
             fast = nums[nums[fast]]
@@ -12,10 +12,14 @@ class Solution:
         
         slow = nums[0]
 
+        # Check if there is a cycle by slow x1 and fast x1
         while slow != fast:
             slow = nums[slow]
             fast = nums[fast]
 
         return slow
-        
 
+        # SC -> O(1)
+        # TC -> O(N)
+        
+        
