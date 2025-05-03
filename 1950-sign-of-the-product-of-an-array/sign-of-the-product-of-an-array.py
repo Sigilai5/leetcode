83a391587b29435280f7727ebafcc991b2919968
Brian Sigilai <briansigilai@gmail.com>
2025-05-03 22:10:49 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (21.86%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.py
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.py
@@ -1,17 +1,14 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
-        sign = 1
+        count_negative = 0
 
         for num in nums:
             if num == 0:
                 return 0
             
             if num < 0:
-                sign = -sign
+                count_negative += 1
         
-        return sign
 
-
-        # SC -> O(N)
-        # TC -> O(N)
+        return 1 if count_negative % 2 == 0 else -1
         
