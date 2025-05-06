f65dc95e7d5fbcc30d2ed5ad54a4662e77950e38
Brian Sigilai <briansigilai@gmail.com>
2025-05-06 23:10:26 +0300

    Time: 2182 ms (5.01%) | Memory: 20.2 MB (92.45%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,38 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        unique = []
+        result = set()
 
         nums.sort()
 
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-
             j = i+1
             k = len(nums) - 1
 
             while j < k:
-                if i !=j and i != k and j != k and nums[i] + nums[j] + nums[k] == 0:
-                    unique.append([nums[i],nums[j],nums[k]])
+                if i != j and i != k and j != k and nums[i] + nums[j] + nums[k] == 0:
+                    result.add((nums[i],nums[j],nums[k]))
                     j+=1
                     k-=1
-
-                    while j < k and nums[j] == nums[j-1]:
-                        j+=1
-
-                    while j < k and nums[k] == nums[k+1]:
-                        k-=1 
-
-                elif i !=j and i != k and j != k and nums[i] + nums[j] + nums[k] > 0:
+                elif i != j and i != k and j != k and nums[i] + nums[j] + nums[k] > 0:
                     k-=1
                 else:
                     j+=1
-
         
-        return unique
+        return list(result)
 
-        # SC -> O(1)
-        # TC -> O(N*M)
-                
-            
+    # SC -> O(N)
+    # TC -> O(N)
         
