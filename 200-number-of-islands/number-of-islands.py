574e153e13f3cf5d6f5d2ce4b645af331dee8a75
Brian Sigilai <briansigilai@gmail.com>
2025-05-14 23:53:33 +0300

    Time: 246 ms (61.26%) | Memory: 20.3 MB (58.13%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,12 +1,9 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-
         count = 0
 
-        for i in range(rows):
-            for j in range(cols):
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
                 if grid[i][j] == "1":
                     count += self.sink(grid,i,j)
 
