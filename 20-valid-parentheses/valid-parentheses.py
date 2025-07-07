fc17e97d40fc761352e574ce7562ac8ef7df780e
Brian Sigilai <briansigilai@gmail.com>
2025-07-07 08:54:36 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (32.18%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,19 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        if len(s) == 1: return False
+
         stack = []
 
-        for ch in s:
-            if ch == "(" or ch == "{" or ch == "[":
-                stack.append(ch)
-            elif stack and ch == ")" and stack[-1] == "(":
+        for par in s:
+            if par == "(" or par == "[" or par == "{":
+                stack.append(par)
+            elif stack and par == ")" and stack[-1] == "(":
                 stack.pop(-1)
-            elif stack and ch == "}" and stack[-1] == "{":
+            elif stack and par == "]" and stack[-1] == "[":
                 stack.pop(-1)
-            elif stack and ch == "]" and stack[-1] == "[":
+            elif stack and par == "}" and stack[-1] == "{":
                 stack.pop(-1)
             else:
                 return False
-
         
         if len(stack) == 0: return True
 
@@ -21,4 +22,3 @@ class Solution:
 
         # SC -> O(N)
         # TC -> O(N)
-        
