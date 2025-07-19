ea2cdea22c5a08d9865842bd7ff9b9c7c3437c66
Brian Sigilai <briansigilai@gmail.com>
2025-07-19 23:54:39 +0300

    Time: 235 ms (80.87%) | Memory: 20.1 MB (82.31%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,27 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+
         count = 0
 
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == "1":
                     count += self.sink(grid,i,j)
-
-        return count
         
+        return count
     
     def sink(self,grid,i,j):
-        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == "0":
+        if (i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] == "0"):
             return 0
 
         grid[i][j] = "0"
-
         self.sink(grid,i+1,j)
         self.sink(grid,i-1,j)
-        self.sink(grid,i,j-1)
         self.sink(grid,i,j+1)
-
+        self.sink(grid,i,j-1) 
         return 1
-        
-        # SC -> O(1)
-        # TC -> O(M * N)
+
+
+    
+    # SC -> O(N) for recursive stack frames
+    # TC -> O(N)
