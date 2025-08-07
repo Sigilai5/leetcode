bceb5ed07f8e4ec12af9798aba0c59d63bbce17a
Brian Sigilai <briansigilai@gmail.com>
2025-08-07 08:10:53 +0300

    Time: 2 ms (19.32%) | Memory: 17.9 MB (88.56%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,29 +1,34 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        if not image or not image[0] or image[sr][sc] == color: return image
+
         rows = len(image)
         cols = len(image[0])
 
-        start_color = image[sr][sc]
 
-        visited = set()
-
-        def dfs(row,col):
-            if row < 0 or row >= len(image) or col < 0 or col >= len(image[0]) or image[row][col] != start_color or (row,col) in visited:
+        def dfs(row,col,start):
+            if (row < 0 or row >= rows or col < 0 or col >= cols or image[row][col] != start):
                 return
 
-            image[row][col] = color 
-            visited.add((row,col))
+            image[row][col] = color
 
-            dfs(row+1,col)
-            dfs(row-1,col)
-            dfs(row,col+1)
-            dfs(row,col-1)
+            dfs(row + 1,col,start)  
+            dfs(row - 1,col,start)
 
+            dfs(row ,col + 1,start)
+            dfs(row ,col - 1,start)
 
         for row in range(rows):
             for col in range(cols):
-                if (row,col) == (sr,sc) and image[row][col] == start_color and (row,col) not in visited:
-                    dfs(row,col)
+                if row == sr and col == sc:
+                    start = image[row][col]
+                    dfs(row, col, start)
 
-
+        
         return image
+
+        # SC -> O(N), recursion stack
+        # TC -> O(R * C)
+
+
+        
