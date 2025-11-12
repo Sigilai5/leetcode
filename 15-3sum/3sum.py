8cecce819736bd13f44f9ea762d53d8f486b4365
Brian Sigilai <briansigilai@gmail.com>
2025-11-12 10:17:48 +0300

    Time: 627 ms (32.96%) | Memory: 20.7 MB (37.30%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,14 +1,19 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         # initialize output array/set
-        output = set()
+        output = []
         # sort nums in ascending order
         nums.sort()
+        print(nums)
         # loop through the array nums
         for i in range(len(nums)):
             # early termination
             if nums[i] > 0:
                 break
+
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
 
         # initialize pointer j at i + 1 and k at last index
             j,k = i + 1, len(nums) - 1 
@@ -16,16 +21,23 @@ class Solution:
             while j < k:
                 if (nums[i] + nums[j] + nums[k]) == 0:
         # append result in output array
-                    output.add((nums[i],nums[j],nums[k]))
+                    output.append([nums[i],nums[j],nums[k]])
                     j+=1
                     k-=1
+
+                    while j < k and nums[j] == nums[j - 1]:
+                        j+=1
+
+                    while j < k and nums[k] == nums[k + 1]:
+                        k-=1
+
                 elif nums[i] + nums[j] + nums[k] < 0:
                     j+=1
                 else:
                     k-=1
         # return the array/set
 
-        return list(output)
+        return output
 
 
         # SC -> O(N)
