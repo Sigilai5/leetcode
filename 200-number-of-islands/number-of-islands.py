4be78d69b78cd7ee4dfc23bf370a1c848ae6160b
Brian Sigilai <briansigilai@gmail.com>
2025-11-18 22:15:20 +0300

    Time: 237 ms (76.67%) | Memory: 20.1 MB (82.30%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,6 +2,20 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         # initialize count to 0
         count = 0
+
+        def sink(grid,i,j):
+                    if(i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]) or grid[i][j] == "0"):
+                        return 0
+                    
+                    grid[i][j] = "0" 
+
+                    sink(grid,i+1,j) # up
+                    sink(grid,i-1,j) # down
+                    sink(grid,i,j+1) # right
+                    sink(grid,i,j-1) # left
+
+                    return 1   
+
         # traverse through the rows and columns
         for i in range(len(grid)):
             for j in range(len(grid[i])):
@@ -9,22 +23,11 @@ class Solution:
                 if grid[i][j] == "1":     
                 # add 1 to the count
                 # sink all land recursively
-                    count += self.sink(grid,i,j) 
+                    count += sink(grid,i,j)  
 
         return count
     
-    def sink(self,grid,i,j):
-        if(i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]) or grid[i][j] == "0"):
-            return 0
-        
-        grid[i][j] = "0" 
-
-        self.sink(grid,i+1,j) # up
-        self.sink(grid,i-1,j) # down
-        self.sink(grid,i,j+1) # right
-        self.sink(grid,i,j-1) # left
-
-        return 1       
+            
 
         # TC -> O(M * N)
         # SC -> O(M * N)
