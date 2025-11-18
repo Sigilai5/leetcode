548106581c583560d5d262ed7f69c22de22c7ad0
Brian Sigilai <briansigilai@gmail.com>
2025-11-18 22:12:28 +0300

    Time: 245 ms (59.87%) | Memory: 20 MB (95.36%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,42 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid or not grid[0]: return 0
-
+        # initialize count to 0
         count = 0
-
-        rows = len(grid)
-        cols = len(grid[0])
-
-        visited = set()
-
-
-        def dfs(row,col):
-            if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] != "1" or (row,col) in visited:
-                return 
-            
-            grid[row][col] = 0
-
-            visited.add((row,col))
-
-            dfs(row + 1, col)
-            dfs(row - 1, col)
-            dfs(row, col + 1)
-            dfs(row, col - 1)
-
-            return 1
-
-
-        
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == "1" and (row,col) not in visited:
-                    count += dfs(row,col)
-
+        # traverse through the rows and columns
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                # look for land
+                if grid[i][j] == "1":     
+                # add 1 to the count
+                # sink all land recursively
+                    count += self.sink(grid,i,j) 
 
         return count
-
-        # SC -> O(1)
-        # TC -> O(R * C)
-
-
+    
+    def sink(self,grid,i,j):
+        if(i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]) or grid[i][j] == "0"):
+            return 0
         
+        grid[i][j] = "0" 
+
+        self.sink(grid,i+1,j) # up
+        self.sink(grid,i-1,j) # down
+        self.sink(grid,i,j+1) # right
+        self.sink(grid,i,j-1) # left
+
+        return 1       
+
+        # TC -> O(M * N)
+        # SC -> O(M * N)
