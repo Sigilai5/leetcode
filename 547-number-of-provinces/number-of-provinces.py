2107b8863bfb29de50b3e0767d618ce5ca5263be
Brian Sigilai <briansigilai@gmail.com>
2025-11-20 12:27:02 +0300

    Time: 3 ms (89.26%) | Memory: 19.7 MB (5.82%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,33 +1,35 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        # DEPTH FIRST SEARCH
         if not isConnected: return 0
 
-        provinces = 0
-
+        # get length of the city
         n = len(isConnected)
 
+        # mark all cities as unvisted
         visited = [False] * n
+
+        # initialize province count
+        count = 0
 
         def dfs(city):
             for neighbor in range(n):
-                if isConnected[city][neighbor] == 1 and not visited[neighbor]:
+                if isConnected[city][neighbor] == 1 and visited[neighbor] == False:
                     visited[neighbor] = True
 
                     dfs(neighbor)
-                    
 
 
         for city in range(n):
             if not visited[city]:
                 visited[city] = True
 
-                provinces += 1
+                count +=1
 
                 dfs(city)
-        
 
-        return provinces
         
+        return count
 
-        # SC -> O(N)
+        # SC -> O(N) , recursive stack frames
         # TC -> O(N * N)
