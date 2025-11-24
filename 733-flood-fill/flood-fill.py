77c30f2bde9a05380cade33a6016375c257eeb2a
Brian Sigilai <briansigilai@gmail.com>
2025-11-24 08:11:02 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (51.90%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,34 +1,30 @@
+from typing import List
+
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        if not image or not image[0] or image[sr][sc] == color: return image
+        if not image or not image[0] or image[sr][sc] == color:
+            return image
 
-        rows = len(image)
-        cols = len(image[0])
-
-
-        def dfs(row,col,start):
-            if (row < 0 or row >= rows or col < 0 or col >= cols or image[row][col] != start):
+        def dfs(image, i, j, start):
+            if (i < 0 or j < 0 or i >= len(image) or j >= len(image[0]) or 
+                image[i][j] != start):
                 return
 
-            image[row][col] = color
+            image[i][j] = color
+            # up
+            dfs(image, i - 1, j, start)
+            # down
+            dfs(image, i + 1, j, start)
+            # left
+            dfs(image, i, j - 1, start)
+            # right
+            dfs(image, i, j + 1, start)
 
-            dfs(row + 1,col,start)  
-            dfs(row - 1,col,start)
+        for i in range(len(image)):
+            for j in range(len(image[i])):
+                if i == sr and j == sc:  # Fixed: j == sc
+                    start = image[i][j]
+                    dfs(image, i, j, start)
+                    return image
 
-            dfs(row ,col + 1,start)
-            dfs(row ,col - 1,start)
-
-        for row in range(rows):
-            for col in range(cols):
-                if row == sr and col == sc:
-                    start = image[row][col]
-                    dfs(row, col, start)
-
-        
         return image
-
-        # SC -> O(N), recursion stack
-        # TC -> O(R * C)
-
-
-        
