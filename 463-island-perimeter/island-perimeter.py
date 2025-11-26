78f34a491e2666f49a3d2a32984be52e4335b3f7
Brian Sigilai <briansigilai@gmail.com>
2025-11-27 00:02:26 +0300

    Time: 95 ms (43.42%) | Memory: 18.5 MB (43.23%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -2,39 +2,36 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         if not grid: return 0
 
-        rows = len(grid)
-              
 
-        def dfs(grid,r,c):
-            if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[r]) or grid[r][c] == 0:
+        def dfs(grid,i,j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] == 0:
                 return 1
 
-            if grid[r][c] == 2:
-                return 0 
+            if grid[i][j] == 2: 
+                return 0    
 
-            grid[r][c] = 2
-
+            grid[i][j] = 2
+            
             perimeter = 0
-            perimeter += dfs(grid,r - 1,c)
-            perimeter += dfs(grid,r + 1,c) 
-            perimeter += dfs(grid,r,c - 1)
-            perimeter += dfs(grid,r ,c + 1)
+            # up
+            perimeter += dfs(grid,i - 1,j)
+            # down
+            perimeter += dfs(grid,i + 1,j)
+            # left
+            perimeter += dfs(grid,i,j - 1)
+            # right
+            perimeter += dfs(grid,i,j + 1)
 
             return perimeter
 
-
-            # SC -> O(R * C)
-            # TC -> O(R * C)
-
-
-
-
-
-        for r in range(len(grid)):
-            for c in range(len(grid[r])):
-                if grid[r][c] == 1:
-                    return dfs(grid,r,c)
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    return dfs(grid,i,j)
 
         
         return 0
+
+        # SC -> O(M * N)
+        # TC -> O(M * N)
         
