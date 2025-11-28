8d0ae42baafed75a17494a5f84162be181ac2ec5
Brian Sigilai <briansigilai@gmail.com>
2025-11-28 23:37:54 +0300

    Time: 111 ms (36.10%) | Memory: 18.6 MB (38.87%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,37 +1,29 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        if not grid: return 0
-
-
+   
         def dfs(grid,i,j):
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] == 0:
                 return 1
 
-            if grid[i][j] == 2: 
-                return 0    
+            if grid[i][j] == 2: return 0
 
             grid[i][j] = 2
-            
-            perimeter = 0
-            # up
-            perimeter += dfs(grid,i - 1,j)
-            # down
-            perimeter += dfs(grid,i + 1,j)
-            # left
-            perimeter += dfs(grid,i,j - 1)
-            # right
-            perimeter += dfs(grid,i,j + 1)
 
-            return perimeter
+            per = 0
+
+            per += dfs(grid,i - 1,j)
+            per += dfs(grid,i + 1,j)
+            per += dfs(grid,i,j - 1)
+            per += dfs(grid,i,j + 1)
+
+            return per
 
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 1:
                     return dfs(grid,i,j)
-
         
+
         return 0
 
-        # SC -> O(M * N)
-        # TC -> O(M * N)
         
