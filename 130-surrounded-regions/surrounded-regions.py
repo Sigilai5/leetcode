caa14eaadbfada62fd311cb293ee56c5382ef48d
Brian Sigilai <briansigilai@gmail.com>
2025-11-30 23:56:20 +0300

    Time: 1 ms (89.99%) | Memory: 21.5 MB (87.50%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,37 +1,39 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        rows = len(board)
-        cols = len(board[0])
+        if not board: return board
+
+        rows, cols = len(board), len(board[0])
 
 
-
-        def dfs(row,col):
+        def dfs(row, col):
             if row < 0 or row >= rows or col < 0 or col >= cols or board[row][col] != "O":
                 return 
 
             board[row][col] = "B"
 
-            dfs(row,col + 1)
-            dfs(row,col - 1)
-            dfs(row + 1, col)
             dfs(row - 1, col)
+            dfs(row + 1, col)
+            dfs(row, col - 1)
+            dfs(row, col + 1)
 
 
-        # Mark all "O" in border
+        # Mark all Os in boarder as B
 
+        '''First and last column'''
         for row in range(rows):
             if board[row][0] == "O":
                 dfs(row,0)
             if board[row][cols - 1] == "O":
                 dfs(row,cols - 1)
+
         
+        '''First and last row'''
         for col in range(cols):
             if board[0][col] == "O":
                 dfs(0,col)
-            if board[rows - 1][col]:
-                dfs(rows - 1,col)
+            if board[rows - 1][col] == "O":
+                dfs(rows - 1, col)   
         
-        # Replace all "O" that's inside with "X" and mark all "B" in border with "O"
 
         for row in range(rows):
             for col in range(cols):
@@ -39,11 +41,3 @@ class Solution:
                     board[row][col] = "X"
                 elif board[row][col] == "B":
                     board[row][col] = "O"
-
-
-        # SC -> O(R * C)
-        # TC -> O(R * C)
-
-
-        
-        
