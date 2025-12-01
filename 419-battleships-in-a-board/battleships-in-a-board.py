b2959707e279503ed213998e3d8c7e030a27ebf9
Brian Sigilai <briansigilai@gmail.com>
2025-12-01 23:58:51 +0300

    Time: 7 ms (24.15%) | Memory: 20 MB (68.04%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.py
+++ 419-battleships-in-a-board/battleships-in-a-board.py
@@ -5,27 +5,37 @@ class Solution:
         ships = 0
 
         def sink(row,col):
-            if row < 0 or row >= rows or col < 0 or col >= cols or board[row][col] != "X":
-                return 0 
+            if row < 0 or col < 0 or row >= rows or col >= cols or board[row][col] != "X":
+                return 0
 
             board[row][col] = "."
 
-            sink(row - 1,col)
-            sink(row + 1,col)
-            sink(row,col - 1)
-            sink(row,col + 1)
+            queue = deque()
+            queue.append((row,col))
 
+            while queue:
+                r,c = queue.popleft()
+
+                directions = [[0,-1],[-1,0],[1,0],[0,1]]
+
+                for dr,dc in directions:
+                    nr,nc = r + dr, c + dc
+
+                    if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == "X":
+                        board[nr][nc] = "."
+                        queue.append((nr,nc))
+            
             return 1
+
+
 
         for row in range(rows):
             for col in range(cols):
-                if board[row][col] == "X":
-                    ships += sink(row,col)
+                ships+=sink(row,col)
 
-
+        
         return ships
 
         # SC -> O(M * N)
         # TC -> O(M * N)
-
         
