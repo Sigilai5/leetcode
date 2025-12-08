66d5bd94c6cce6e5c691142d59f0a108bb9fb11a
Brian Sigilai <briansigilai@gmail.com>
2025-12-08 22:57:54 +0300

    Time: 226 ms (94.32%) | Memory: 20 MB (82.19%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,23 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, cols = len(grid), len(grid[0])
+        rows,cols = len(grid),len(grid[0])
 
         count = 0
 
-        def dfs(row,col):
-            if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] != "1":
+        def dfs(i,j):
+            if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] != "1":
                 return 0
 
-            grid[row][col] = "0"
+            grid[i][j] = "0"
+
             queue = deque()
-            queue.append((row,col))
+
+            queue.append((i,j))
+
+            directions = [[-1,0],[1,0],[0,-1],[0,1]]
 
             while queue:
                 r,c = queue.popleft()
 
-                directions = [[-1,0],[1,0],[0,-1],[0,1]]
-
-                for dr, dc in directions:
+                for dr,dc in directions:
                     new_r = r + dr
                     new_c = c + dc
 
@@ -25,22 +27,17 @@ class Solution:
                         grid[new_r][new_c] = "0"
                         queue.append((new_r,new_c))
             
-
             return 1
 
 
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == "1":
-                    count += dfs(row,col)
-
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1":
+                    count += dfs(i,j)
         
+
         return count
 
-        # TC -> O(M * N)
         # SC -> O(M * N)
-
-
-
-
+        # TC -> O(M * N)
         
