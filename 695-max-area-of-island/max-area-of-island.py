9a9df2710bd18f4c4a7a9cd632d6f390b21e7d5f
Brian Sigilai <briansigilai@gmail.com>
2025-12-10 21:55:05 +0300

    Time: 14 ms (90.90%) | Memory: 18.4 MB (52.10%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,37 +1,32 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid: return 0
-
         max_area = 0
-        
-        def sink(grid,i,j):
-            if(i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]) or grid[i][j] == 0):
+        rows,cols = len(grid), len(grid[0])
+
+        def dfs(row,col):
+            if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] != 1:
                 return 0
 
-            grid[i][j] = 0
+            grid[row][col] = 0
             area = 1
-            # up
-            area += sink(grid,i-1,j)
-            # down
-            area += sink(grid,i+1,j)
-            # left
-            area += sink(grid,i,j-1)
-            # right
-            area += sink(grid,i,j+1)
+
+            area += dfs(row - 1,col)
+            area += dfs(row + 1,col)
+            area += dfs(row,col - 1)
+            area += dfs(row,col + 1)
 
             return area
 
 
-
-
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == 1:
-                    max_area = max(max_area,sink(grid,i,j))
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1:
+                    max_area = max(max_area,dfs(row,col))
 
         
-        return  max_area
 
-        # SC -> O(N * M)
-        # TC -> O(N * M)
+        return max_area
+
+        # SC -> O(M * N)
+        # TC -> O(M * N)
         
